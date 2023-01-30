0ba42d36807d5a7687c16d9759477d81c5c0e8cb
yonggyo1125 <yonggyo00@kakao.com>
2023-01-30 09:39:51 +0900

    전략패턴 강의 소스

---
+++ 3. 디자인 패턴(Design Pattern)/1. 전략패턴/source/strategy/RubberDuck.java
@@ -0,0 +1,17 @@
+package strategy;
+
+public class RubberDuck extends Duck {
+    public RubberDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = () -> System.out.println("Squeak");
+    }
+
+    public RubberDuck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void display() {
+        System.out.println("I'm a rubber duckie");
+    }
+}
